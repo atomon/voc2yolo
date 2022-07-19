45e18c32996ccbcc2920943d6baf141c0c3ce3c8
atomon <robosystem_126@yahoo.co.jp>
2022-07-19 00:36:20 +0000

    added augmentation

---
+++ augmentation.py
@@ -0,0 +1,9 @@
+import augmentation as A
+import cv2
+
+transform = A.Compose([
+    A.RandomCrop(width=450, height=450),
+    A.HorizontalFlip(p=0.5),
+    A.RandomBrightnessContrast(p=0.2),
+], bbox_params=A.BboxParams(format='coco'))
+
